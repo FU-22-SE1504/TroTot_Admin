b6925881ce507d12dc591bac5e814abf275d7329
Khangtt1110 <101918984+Khangtt1110@users.noreply.github.com>
2022-10-27 14:53:41 +0700

    [Feat] User manager

--- TroTot_Admin/Model/User.cs
+++ TroTot_Admin/Model/User.cs
@@ -5,18 +5,29 @@ namespace TroTot_Admin.Model
     public class User
     {
         [Key]
+        [Display(Name = "ID")]
         public int user_id { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Username is required")]
+        [Display(Name = "Username")]
+        [StringLength(maximumLength: 20, MinimumLength = 8, ErrorMessage = "Username must be greater than 8 and lower than 20 character")]
         public string username { get; set; }
         [Required]
+        [Display(Name = "Password")]
+
         public string password { get; set; }
         [Required]
+        [Display(Name = "Email Address")]
         public string email { get; set; }
+        [Display(Name = "Phone Number")]
+
         public string? phone_number { get; set; }
+        [Display(Name = "Full Name")]
         public string? full_name { get; set; }
         [Required]
+        [Display(Name = "Date Created")]
         public DateTime create_at { get; set; }
         [Required]
+        [Display(Name = "Role")]
         public int role_id { get; set; }
         public string avatar { get; set; }
     }
--- TroTot_Admin/Pages/Account/Index.cshtml.cs
+++ TroTot_Admin/Pages/Account/Index.cshtml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -26,6 +27,7 @@ namespace TroTot_Admin.Pages.Account
             if (_context.User != null)
             {
                 User = await _context.User.ToListAsync();
+                //String decode = Encoding.UTF8.GetString(Convert.FromBase64CharArray(U))
             }
         }
     }
